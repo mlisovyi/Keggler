82c392af67590ea50d61626773864c43e35779ae
mlisovyi <mlisovyi@gmail.com>
2018-09-13 10:14:50 +0200

    change the way python requirement is posed

--- setup.py
+++ setup.py
@@ -11,12 +11,13 @@ setup(
     license='LICENSE.txt',
     description='A personal kaggle toolkit',
     long_description=open('README.md').read(),
+    python_requires='~=3.5',
     install_requires=[
         "numpy >= 1.14",
         "pandas >= 0.22.0",
         "scikit-learn >= 0.19.1",
         "lightgbm >= 2.1.0",
-        "python >= 3.5",
+#        "python >= 3.5",
         "matplotlib >= 2.2.0"
     ],
 )
